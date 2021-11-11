b43c76db0abc267f06a42c4746628b439ce29706
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2021-11-11 18:46:00 +0000

    First commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const renderRandomTerm = (terms) => {
+  const titleElement = document.querySelector('h1.title');
+  const descriptionElement = document.querySelector('p.description');
+
+  const titles = Object.keys(terms);
+
+  const randomTitle = titles[Math.floor(Math.random() * titles.length)];
+  const randomDescription = terms[randomTitle];
+
+  titleElement.innerText = `🏌️‍♂️ ${randomTitle}`;
+  descriptionElement.innerText = randomDescription;
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  let terms = {};
+
+  if (localStorage.hasOwnProperty("GolfGlossary")) {
+    terms = JSON.parse(localStorage.getItem("GolfGlossary"));
+    renderRandomTerm(terms);
+  }
+
+  const seeMoreButton = document.querySelector("button.see-more-button");
+  seeMoreButton.addEventListener("click", () => renderRandomTerm(terms));
+});
